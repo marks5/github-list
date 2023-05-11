0c11e082956e4276b50df7fb0ed3f352a10c147c
gmrksao <119072764+gmrksao@users.noreply.github.com>
2023-05-11 14:23:56 -0300

    GithubAPI

---
+++ app/src/main/java/com/example/githublist/data/GithubAPI.kt
@@ -0,0 +1,43 @@
+package com.example.githublist.data
+
+import com.example.githublist.model.Repository
+import com.example.githublist.model.User
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.*
+
+interface GithubAPI {
+
+    @GET("users")
+    @Headers("Accept: application/json", "Authorization: Bearer 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")
+    suspend fun getUsers(@Path("since") page: Int, @Path("per_page") per_page: Int): List<User>
+
+    @GET("users/{username}")
+    @Headers("Accept: application/json", "Authorization: Bearer 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")
+    suspend fun searchUser(@Path("username") username: String): User
+
+    @GET("users/{username}/repos")
+    @Headers("Accept: application/json", "Authorization: Bearer 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")
+    suspend fun getUserRepos(@Path("username") username: String): List<Repository>
+
+    companion object {
+        private const val BASE_URL = "https://api.github.com/"
+
+        fun create(): GithubAPI {
+            val logger = HttpLoggingInterceptor()
+            logger.level = HttpLoggingInterceptor.Level.BASIC
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(GithubAPI::class.java)
+        }
+    }
+}
